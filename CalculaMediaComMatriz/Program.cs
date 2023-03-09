9c5aec90dc6361881c6e368197120a4f85925be0
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-08 23:38:09 -0300

    Add project files.

---
+++ CalculaMediaComMatriz/Program.cs
@@ -0,0 +1,27 @@
+﻿float[,] mat = { { 3, 2, 0 }, { 4, 5, 0 }, { 9, 8, 0}, { 4, 5, 0 }, { 9, 8, 0 } };
+
+void CalculaMedia(float[,] mat)
+{
+    for (int linha = 0; linha < mat.GetLength(0); linha++)
+    {
+        for (int coluna = 0; coluna < (mat.GetLength(1) - 1); coluna++)
+            mat[linha, (mat.GetLength(1) - 1)] += mat[linha, coluna];
+
+        mat[linha, (mat.GetLength(1) - 1)] /= (mat.GetLength(1) - 1);
+    }
+}
+
+
+CalculaMedia(mat);
+
+for (int linha = 0; linha < mat.GetLength(0); linha++)
+{
+    for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+        Console.Write("[" + mat[linha, coluna] + "] ");
+    
+    Console.Write("\n");
+}
+    
+
+
+
